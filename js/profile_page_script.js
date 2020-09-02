2b1b439241c6c061274f13971afa0119581b6ecb
THANGA MANICKAM M <thanga.manickam5@gmail.com>
2020-09-02 16:59:36 +0530

    Inital version of profile page

---
+++ js/profile_page_script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.avatar').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+
+    $("body").on('click', '.panel[data-href]', function(){
+        $($(this).data('href')).modal();
+    });
+});
+
+
+
