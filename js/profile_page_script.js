d88229156e821b5235c7396121fb3e9a9758049d
THANGA MANICKAM M <thanga.manickam5@gmail.com>
2020-09-17 15:45:06 +0530

    Fixed padding. Added cancel button, lot of js changes for checking user access.

--- js/profile_page_script.js
+++ js/profile_page_script.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-
+$(document).ready(function() {    
     
     var readURL = function(input) {
         if (input.files && input.files[0]) {
@@ -12,13 +11,57 @@ $(document).ready(function() {
             reader.readAsDataURL(input.files[0]);
         }
     }
+    var getUrlParams = function()
+    {
+        const queryString = window.location.search.substring(1);
+        params = {}
+        queryString.split('&').forEach(function (queries)
+        {
+            key_value = queries.split('=');
+            params[key_value[0]] = params[key_value[1]];
+        })
+        return queryString
+    }
+
+    var validateUser = function()
+    {
+        if (sessionStorage.getItem('rollno') == null)
+        {
+            return false;
+        }
+        userroll = sessionStorage.getItem('rollno')
+        profileroll = getUrlParams['rollno']
+        if(userroll!=profileroll)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    if (validateUser())
+    {
+        console.log(getUrlParams())
+        $('button').hide();
+    }
+    var editButtons = $('button[name ="edit-button"]')
+    var saveButtons = $('button[name ="save-button"]')
+    var resetButtons = $('button[name ="reset-button"]')
+    var cancelButtons = $('button[name ="cancel-button"]')
     $(':input').attr('readonly','readonly');
-    $("#edit-button").click(function(){
+    saveButtons.hide()
+    resetButtons.hide();
+    cancelButtons.hide();
+    editButtons.click(function(){
+        $(':input').prop("readonly", false);
+        saveButtons.show();
+        resetButtons.show();
+        cancelButtons.show();
+        editButtons.hide();
+      });
+      saveButtons.click(function(){
         $(':input').prop("readonly", false);
       });
-      $("#save-button").click(function(){
-        $(':input').prop("readonly", false);
-      }); 
+    
     $(".file-upload").on('change', function(){
         readURL(this);
     });
@@ -26,6 +69,8 @@ $(document).ready(function() {
     $("body").on('click', '.panel[data-href]', function(){
         $($(this).data('href')).modal();
     });
+
+    
 });
 
 
